f51618eb56f212354bfb0d917b107af18246e85e
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-29 14:57:00 -0700

    Update landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Landing = () => {
   return (
     <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
-      <h3>
-        If you sign up for a new account, you will have the ability to sign in
-        and see your super secret dashboard.
-      </h3>
+      <h1>Eorzea Journal</h1>
+      <p>A Final Fantasy XIV Online character tracker.</p>
+      <Link to={'/signin'}>Sign in</Link>
+      <Link to={'/signup'}>Sign up</Link>
     </main>
   );
 };
