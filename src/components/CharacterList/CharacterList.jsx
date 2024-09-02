e5f038122228f07ab5d76fe91db86932f8be6bff
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-02 15:53:05 -0700

    Refactor character list to conditionally render if a user has made a character or not

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 
 const CharacterList = ({ user, characters, updateSelectedCharacter }) => {
+    const characterList = characters.map(character =>
+        <a key={character._id} onClick={() => updateSelectedCharacter(character)}><li>{character.name}</li></a>
+    );
     return (
-        <ul>
-            {characters.map(character => (
-                <a key={character._id} onClick={() => updateSelectedCharacter(character)}><li>{character.name}</li></a>
-            ))}
-        </ul>
+        <>
+            {!characters.length ? <p>No characters yet.</p> : <ul>{characterList}</ul>}
+        </>
     );
 };
 
